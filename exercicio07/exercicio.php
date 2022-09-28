519694c9ada3135ed0108fa2e40a077dcf2a0f05
MarcosSilva13 <marcosslp45@gmail.com>
2022-09-28 11:01:55 -0300

    Exercicio 07

---
+++ exercicio07/exercicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Atividade 07</title>
+</head>
+<body>
+    <header>***Atividade 07***</header>
+
+    <div class="botoes">
+        <button class="botao1">Botão 1</button>
+        <button class="botao2">Botão 2</button>
+        <button class="botao3">Botão 3</button>
+        <button class="botao4">Botão 4</button>
+        <button class="botao5">Botão 5</button>
+    </div>
+
+</body>
+</html>
