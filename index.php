2b544b14d0a1c44e3d74de7244797c61a9d8161c
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-03 17:02:18 -0300

    atividades da aula

--- index.php
+++ index.php
@@ -9,6 +9,25 @@
 </head>
 
 <body>
+
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Loja</li>
+                <li>Sobre</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div>Div 01</div>
+        <div>Div 02</div>
+        <div>Div 03</div>
+        <div>Div 04</div>
+    </main>
+
+
     <h1>***Lista de Estudantes***</h1>
     <table>
         <tr>
@@ -16,7 +35,6 @@
             <th>Nome</th>
             <th>Idade</th>
             <th>Cpf</th>
-            <th>Ações</th>
             <th>Ações</th>
         </tr>
 
@@ -32,8 +50,8 @@
             echo '<td>' . $valores['Name'] . '</td>';
             echo '<td>' . $valores['Age'] . '</td>';
             echo '<td>' . $valores['Cpf'] . '</td>';
-            echo '<td><a id="editar" href="form.php?Ra=' . $valores['Ra'] . '&acao=2">Editar</a></td>';
-            echo '<td>
+            echo '<td id="acoes"><a id="editar" href="form.php?Ra=' . $valores['Ra'] . '&acao=2">Editar</a>
+           
                 <form action="excluirEstudante.php" method="POST">
                 <input type="hidden" name="Ra" id="Ra" value="' . $valores['Ra'] . '"/>
                 <input type="submit" id="excluir" value="Excluir"/>
@@ -46,17 +64,6 @@
     <br>
     <a href="form.php?acao=1" id="cad">Cadastrar</a>
     <a href="./visao/listagemDisc.php" id="disc">Ver Disciplinas</a>
-    <br>
     
-    <button class="botaoTipo1">Botão 1</button>
-    <button class="botaoTipo1">Botão 2</button>
-    <button class="botaoTipo2">Botão 3</button>
-    <button class="botaoTipo2">Botão 4</button>
-    <button class="botaoTipo3">Botão 5</button>
-
-    <p id="p1">Paragráfo com ID</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum totam autem, dolorem molestiae cumque placeat possimus est repellendus. 
-        Veniam deleniti vitae asperiores distinctio nulla ipsum? Quam architecto quisquam voluptate exercitationem!</p>
-
 </body>
 </html>
