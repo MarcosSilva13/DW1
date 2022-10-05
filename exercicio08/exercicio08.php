a95942f32a719492824cc1185f093c26d77cdd2f
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-05 11:01:28 -0300

    Iniciando exercicio 08

---
+++ exercicio08/exercicio08.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style08.css"/>
+    <title>Exercicio 08</title>
+</head>
+<body>
+
+    <div class="campo">
+        <div class="img">
+            <h2>Naruto</h2>
+            <img src="http://wonderfulengineering.com/wp-content/uploads/2016/01/Naruto-Wallpaper-21.jpg">
+        </div>
+        <div class="img">
+            <h2>Sasuke</h2>
+            <img src="https://coolthemestores.com/wp-content/uploads/2020/12/sasuke-uchiha-wallpaper-background.jpg">
+        </div>
+        <div class="img">
+            <h2>Kakashi</h2>
+            <img src="https://images5.alphacoders.com/665/thumb-1920-665373.jpg">
+        </div>
+        <div class="img">
+            <h2>Itachi</h2>
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/afd4dc66-4cbe-4309-9526-0c5578e7d3a3/deznudl-904bb3d6-7948-422c-bfa8-271e6cfb3938.jpg/v1/fill/w_1280,h_720,q_75,strp/itachi_uchiha_wallpaper_by_jorgeadesigner_deznudl-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvYWZkNGRjNjYtNGNiZS00MzA5LTk1MjYtMGM1NTc4ZTdkM2EzXC9kZXpudWRsLTkwNGJiM2Q2LTc5NDgtNDIyYy1iZmE4LTI3MWU2Y2ZiMzkzOC5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.q7uFv5E1rz3hu1ehP7obtDkIWCkFTTlGyg0s3REvQq0">
+        </div>
+    </div>
+</body>
+</html>
