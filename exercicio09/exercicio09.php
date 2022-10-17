1ecb8998af6e60742321d800c2376ac44d037998
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-17 18:40:50 -0300

    Pequenos ajustes

--- exercicio09/exercicio09.php
+++ exercicio09/exercicio09.php
@@ -7,11 +7,11 @@
     <title>Exercicio 09</title>
 </head>
 <body>
-    <div class="retangulos">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
+    <div class="paragrafos">
+        <p>P1</p>
+        <p>P2</p>
+        <p>P3</p>
+        <p>P4</p>
     </div>
 </body>
 </html>
